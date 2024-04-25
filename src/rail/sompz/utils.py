c732ac4aacebcf6beb2c1634a80f7f72da5100f3
Justin T Myles <jmyles@berkeley.edu>
2024-04-24 22:29:23 -0400

    add function. next step integrate into i/o standards

--- src/rail/sompz/utils.py
+++ src/rail/sompz/utils.py
@@ -34,3 +34,40 @@ def luptize(flux, var, s, zp):
     lupt = mu0 - a * np.arcsinh(flux / (2 * b))
     lupt_var = a ** 2 * var / ((2 * b) ** 2 + flux ** 2)
     return lupt, lupt_var
+
+def histogram_from_fullpz(df, key, overlap_weighted, bin_edges, full_pz_end = 6.00, full_pz_npts=601):
+    # TODO I will need to make this consistent with standards in rail
+    '''Preserve bins from Laigle'''
+    dz_laigle = full_pz_end / (full_pz_npts - 1)
+    condition = np.sum(~np.equal(bin_edges, np.arange(0 - dz_laigle/2.,
+                                    full_pz_end + dz_laigle,
+                                    dz_laigle)))
+    assert condition == 0
+    # bin_edges: [-0.005, 0.005], (0.005, 0.015], ... (5.995, 6.005]
+
+    single_cell_hists = np.zeros((len(df), len(key)))
+
+    overlap_weights = np.ones(len(df))
+    if(overlap_weighted):
+        overlap_weights = df['overlap_weight'].values
+
+    single_cell_hists[:,:] = df[key].values
+
+    # normalize sompz p(z) to have area 1
+    dz = 0.01
+    area = np.sum(single_cell_hists, axis=1) * dz
+    area[area == 0] = 1 # some galaxies have pz with only one non-zero point. set these galaxies' histograms to have area 1
+    area = area.reshape(area.shape[0], 1)
+    single_cell_hists = single_cell_hists / area
+
+    # response weight normalized p(z)
+    single_cell_hists = np.multiply(overlap_weights, single_cell_hists.transpose()).transpose()
+
+    # sum individual galaxy p(z) to single cell p(z)
+    hist = np.sum(single_cell_hists, axis=0)
+
+    # renormalize p(z|c)
+    area = np.sum(hist) * dz
+    hist = hist / area
+
+    return hist
