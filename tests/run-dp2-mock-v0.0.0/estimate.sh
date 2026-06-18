54dd678561bb63277c59406a8fea1eec8f59890f
jtmyles <jtmyles@users.noreply.github.com>
2026-06-18 08:01:17 -0700

    add dp2 mock run (input data not good)

---
+++ tests/run-dp2-mock-v0.0.0/estimate.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+##SBATCH --qos=debug 
+##SBATCH --time=00:30:00
+#SBATCH --qos=regular # debug # regular
+#SBATCH --time=06:00:00
+#SBATCH --nodes=1
+#SBATCH -C cpu
+#SBATCH --error="estimate.err"
+#SBATCH --output="estimate.out"
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jmyles@astro.princeton.edu
+
+source $HOME/.bashrc
+module load python
+module swap PrgEnv-${PE_ENV,,} PrgEnv-gnu
+module load PrgEnv-gnu
+module load cray-hdf5-parallel
+echo $HDF5_DIR
+# module load libfabric/1.20.1 # added 2025-08-22 following appearance of msg below after maintenance
+# The following have been reloaded with a version change:
+#  1) libfabric/1.20.1 => libfabric/1.22.0
+
+#### conda activate sompz
+#### conda activate rail_mpi4py
+conda activate rail_sompz 
+
+#ceci test_pipe_FULL_coriparallel_big.yml
+#ceci test_POSTFIX_fullpipe_mod_cardinal.yml
+#ceci $HOME/repositories/rail_sompz/tests/cardinal_estimate.yml
+
+DATADIR_RUN=$(pwd)
+DATADIR_CATALOG=$(python $HOME/repositories/lsst-y1-nz-study/get_run_catalog_datadir.py)
+
+echo $DATADIR_RUN
+echo $DATADIR_CATALOG
+
+ceci cardinal_estimate.yml
+python stack_pzc_into_nz.py # TODO: embed this logic into stages in sompz.py for the one som setup
+
+# python $HOME/repositories/lsst-y1-nz-study/fit-smail-dist-to-rail-sompz-nz.py ./
+# python $HOME/repositories/lsst-y1-nz-study/make-plots-for-sompz-runs.py --datadirs-runs $DATADIR_RUN --datadirs-catalogs $DATADIR_CATALOG
---
+++ tests/run-dp2-mock-v0.0.0/inform.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --qos=regular
+#SBATCH --time=47:59:00
+#SBATCH --nodes=1
+#SBATCH -C cpu
+#SBATCH --error="inform.err"
+#SBATCH --output="inform.out"
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jmyles@astro.princeton.edu
+
+module load python
+module swap PrgEnv-${PE_ENV,,} PrgEnv-gnu
+module load PrgEnv-gnu
+module load cray-hdf5-parallel
+
+#conda activate sompz
+#conda activate rail_mpi4py
+conda activate rail_sompz
+
+#ceci test_oneinformer_bigfiles_jtm.yml
+#ceci test_POSTFIX_fullpipe_mod_cardinal.yml
+ceci cardinal_inform.yml
