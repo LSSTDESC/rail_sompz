797e767f4c063aabcbb8e566387c93ff6c1c5e0f
sschmidt23 <sschmidt@physics.ucdavis.edu>
2026-07-06 16:06:24 -0700

    fix unit tests

--- tests/test_pipelines.py
+++ tests/test_pipelines.py
@@ -6,8 +6,8 @@ from rail.utils.testing_utils import build_and_read_pipeline
 @pytest.mark.parametrize(
     "pipeline_class",
     [
-        'rail.pipelines.estimation.estimate_sompz.EstimateSomPZPipeline',
         'rail.pipelines.estimation.inform_sompz.InformSomPZPipeline',
+        'rail.pipelines.estimation.estimate_sompz.EstimateSomPZPipeline',
     ]
 )
 def test_build_and_read_pipeline(pipeline_class):
